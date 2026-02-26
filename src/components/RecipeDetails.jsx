2aa7a04e39c9cc3b844ae0e5353cde48286ff9d7
Carolyne Yeko <yekocarol2@gmail.com>
2026-02-26 12:31:35 +0300

    Added the back button on the recipe details

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { fetchRecipeById } from "../services/spoonacular";
 
 const RecipeDetails = () => {
     const { id } = useParams();
+    const navigate = useNavigate();
     const [recipe, setRecipe] = useState(null);
     const [loading, setLoading] = useState(true);
 
@@ -20,6 +21,24 @@ const RecipeDetails = () => {
     if (!recipe) return <p className="text-center mt-10">Recipe not found</p>;
 
     return (
+        <div className="container mx-auto p-4">
+            <button 
+            onClick={() => navigate(-1)}
+            className="flex items-center mb-6 bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 transition-all duration-300"
+            >
+                <svg 
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 mr-2"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                >
+                    <path 
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    />
+                </svg>
+                Back
+            </button>
         <div className="container mx-auto p-48">
             <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
             {recipe.image && (
@@ -33,6 +52,7 @@ const RecipeDetails = () => {
             </ul>
             <h2 className="font-bold text-xl mb-2">Instructions</h2>
             <div dangerouslySetInnerHTML={{__html: recipe.instructions }} />
+        </div>
         </div>
     );
 };
