b47052b6579c3032b6371cd7c6a9d3badbe8cdb1
Carolyne Yeko <yekocarol2@gmail.com>
2026-04-27 11:41:47 +0300

    Removed the back button

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -18,25 +18,7 @@ const RecipeCard = ({ recipe }) => (
                 View Details
             </Link>
         </div>
-        <div>
-            <button 
-            onClick={() => navigate(-1)}
-            className="flex items-center mb-6 bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-700 transition-all duration-300"
-            >
-                <svg 
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 mr-2"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                >
-                    <path 
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    />
-                </svg>
-                Back
-            </button>
-        </div>
+        
     </div>
 );
 export default RecipeCard;
